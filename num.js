cb770e85890995146a7ee0b9c9f0a2b8b8612c8b
Ayo <talktojmcvibes@gmail.com>
2024-01-19 15:07:42 +0100

    number validator

--- num.js
+++ num.js
@@ -1,5 +1,5 @@
 function telephoneCheck(str) {
-    var regex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s\-]?)\d{3}([\s\-]?)\d{4}$/;
+    const regex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s\-]?)\d{3}([\s\-]?)\d{4}$/;
   
     return regex.test(str);
   }
