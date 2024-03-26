cb0a0a65734a077e8e11bd480cad458bdf6e761e
Ayo <talktojmcvibes@gmail.com>
2024-03-26 14:54:00 +0100

    fibonacci seq in javascript

--- webdev course/index.js
+++ webdev course/index.js
@@ -1,8 +1,20 @@
-function whoIsPaying(names) {
-    randomNumber = Math.floor(Math.random() * names.length)
-    alert(names[randomNumber] +" is paying for dinner")
+function fibonacciGenerator(n) {
+   let seq = [];
+   if (n < 1) {
+    return
+   }
+   else if (n === 1) {
+    seq.push(0)
+   } else if (n >= 2) {
+    seq.push(0,1);
+    while (n > seq.length) {
+        lastInt = seq.length - 1
+        secondToLast = seq.length - 2
+        seq.push(seq[secondToLast] + seq[lastInt])
+    }
+   }
+   return seq
 
 }
 
-names = ["James", "Angela", "Jack", "Frank", "Betty"]
-whoIsPaying(names)
+alert(fibonacciGenerator(5))
